12c96454d3f4e05c893bfd8f9b91710f826f8cd9
deepanshuTTN <deepanshu.mishra@tothenew.com>
2026-09-25 18:38:42 +0530

    initialized spring project

---
+++ backend/src/main/java/com/epic/sturdyfernacular/SturdyFernacularApplication.java
@@ -0,0 +1,13 @@
+package com.epic.sturdyfernacular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SturdyFernacularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SturdyFernacularApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/epic/sturdyfernacular/SturdyFernacularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epic.sturdyfernacular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SturdyFernacularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
